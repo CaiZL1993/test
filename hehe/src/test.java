b05fb88137fed2fa951f2dfb7f24ddee52dcd932
caizilong <670205813@qq.com>
2018-04-01 20:34:31 +0800

    第一次提交

---
+++ hehe/src/test.java
@@ -0,0 +1,15 @@
+import org.junit.Test;
+
+public class test {
+
+    public static void main(String[] args) {
+        System.out.println("66666");
+    }
+
+
+    @Test
+    public void test() throws  Exception{
+        System.out.println("hahahahaha");
+    }
+
+}
